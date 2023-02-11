1bd22e1261b1b953a17ab5974fd6a8c196b64e90
Emir Öksüz <111661351+oksuzemir@users.noreply.github.com>
2023-02-11 03:58:18 +0300

    minor bug fixes and added favicon to the project

--- src/contents/contents.js
+++ src/contents/contents.js
@@ -1,5 +1,5 @@
 export const Contents = {
-  "/burger-king": {
+  "/coca-cola": {
     title: "Her Altın Kapakta BirCoca-Cola + Coffee Keyfi",
     content:
       "Coca-Cola Coffee, kolanın lezzetini kahve aroması ile bir araya getirir.  Brezilya kahve çekirdeklerinin lezzetiyle aromalanan Coca Cola, geleneksel lezzetine modern bir yorum katarak raflarda yer alır.Yeni tatlara açık olanların beğenisini kazanan kahveli Coca-Cola, kutu tasarımı ile çantada bile kolaylıkla taşınır. Kahve tiryakilerinin bir yudum ferahlık istediği anlarda tercihi olacak ürün, dünyada en çok yankı uyandıran kola edisyonları arasında yer almaktadır. İçeceğin soğuk tüketilmesi önerilir. Buzla beraber içilebilir.Özellikle sıcak yaz günlerinde ferahlatıcı etki gösterir. ",
